e6ebbe1cbb372e09ef32cee7c89a7493eaacb0cd
garghi2000 <garghi2000@hotmail.it>
2022-10-12 16:07:54 +0100

    upgrade with gridspec

--- plot.py
+++ plot.py
@@ -5,128 +5,91 @@ Created on Fri Sep  9 11:25:02 2022
 @author: Gabriele Bertolini
 """
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 import numpy as np
-import pandas as pd
 import math
 
 def _plot1Ddata(Nanonisfile, xChn, yChns, keepAxes):
-    #conversion to pandas dataframe
-    df = pd.DataFrame(Nanonisfile.data)
-    
-    #check on xChn input
-    if xChn != None :
-        if xChn in df.columns:
-            X = xChn
-        elif xChn in range(len(df.columns)):
-            X = df.columns[xChn]
-        else :
-            raise Exception('The parameter xChn must be either None, a string present in '
-                        'the list of the recorded channels names, or an index')
-    else :
-        X = None
-        
-    #check on yChns input
+    data = Nanonisfile.data
+    # some variable for the Width/hight ratio of the plots    
+    unitHeight, goldenRatio = 2, 1.6
+    #extra check for the case xChn is None
+    if '#Index' in data.keys():
+        raise Exception(
+            'The data contains a channel name #Index,'
+            ' this channel might be erased after plot')
+    #check yChns input and convert it in a list of channels name to plot
     if yChns == 'all' :
-        Y = df.columns
-    elif set(yChns).issubset(df.columns):
-        Y = yChns
-    elif set(yChns).issubset(range(len(df.columns))):
-        Y = df.columns[yChns]
+        yNames = list(data.keys())
+    elif yChns in data.keys():
+        yNames = [yChns]
+    elif set(yChns).issubset(data.keys()):
+        yNames = yChns
+    elif set(yChns).issubset(range(len(data.keys()))):
+        yNames = [data.keys()[i] for i in yChns]
     else :
-        raise Exception('The parameter yChns must be a list of column names,'
-                        'a list of idexes, or a string == all')
-    
+        raise Exception(
+            'The parameter yChns must be a list of column names,'
+            'a list of idexes, or a string == all')
+        
+    #check on xChn input
+    if xChn == None :
+        # adding a column of indexes to data dict if xChn is not given
+       xName = '#Index'
+       data['#Index'] = np.arange(len(data[list(data.keys())[0]]))
+    elif xChn in data.keys():
+        xName = xChn
+    elif xChn in range(len(data.keys())):
+        xName = list(data.keys())[xChn]
+    else :
+        raise Exception(
+            'The parameter xChn must be either None, a string present in '
+            'the list of the recorded channels names, or an integer')
+       
     #check on keepAxes input
     if keepAxes == 'True' or keepAxes == True :
-        df.plot(x = X, y = Y)
-        plt.show()       
+        fig, ax = plt.subplots(figsize=(unitHeight*goldenRatio, unitHeight))
+        for yName in yNames:
+            ax.plot(data[xName],data[yName], label = yName)
+            plt.legend(loc="upper left")
+            plt.xlabel(xName)
+        plt.show()
+        
     elif keepAxes == 'False' or keepAxes == False :
-        nAxes = len(Y)
-        # nice way to distribute the subplots on a grid as square as possible
+        nAxes = len(yNames)
+        #Organizing multiplots such that the figure is as square as possible
         nRow = math.floor(math.sqrt(nAxes)) 
         nCol = math.ceil(nAxes/nRow)
-        fig, axes = plt.subplots(nRow, nCol)
-        fig.tight_layout(pad = 1)
-        axr = axes.ravel()
-        i = 0
-        for chName in Y:
-            yAxChName = axr[i]         
-            yAxChName.set_ylabel(chName)
-            i += 1 
-            if X == None or chName == X:
-                df[chName].plot(xlabel = 'Index', ax = yAxChName, y = chName,
-                                figsize =(9, 6))
-            else:
-                df.plot(ax = yAxChName, x = X, y = chName, figsize =(9, 6))
-
-        for ax in axr[nAxes:]:
-            ax.axis('off')
+        #Vector of 2D indexes of the grid in order to use a single loop later
+        gridIdxs = [[i,j] for i in range(nRow) for j in range(nCol)]
+        wr = [unitHeight*goldenRatio]*nCol
+        hr = [unitHeight]*nRow
+        fig = plt.figure(constrained_layout=True, figsize=(sum(wr),sum(hr)))
+        grid = gridspec.GridSpec(
+            nRow, nCol, width_ratios= wr, height_ratios= hr, figure = fig
+            )
+        for i, yName in enumerate(yNames):
+            idx1, idx2 = gridIdxs[i]
+            ax = plt.subplot(grid[idx1, idx2])
+            ax.plot(data[xName], data[yName])
+            plt.ylabel(yName)
+            plt.xlabel(xName)
         plt.show()
     else :
         raise Exception('The parameter keepAxes must be a boolean')
-            
+    # removing extra columns of indexes added before if it exists
+    if '#Index' in data.keys():
+        del data['#Index']
+        
 ############ It needed to perform a plot of y vs x chosen channels ######
 def _plot2Ddata(Nanonisfile, yChns):
     pass
 
 def plot(Nanonisfile, xChn = None, yChns = 'all', keepAxes = False) :
-    
+
     if Nanonisfile.metadata['File extension'] == '.dat': 
         _plot1Ddata(Nanonisfile, xChn, yChns, keepAxes)
         
     elif Nanonisfile.metadata['File extension'] == '.sxm':
         _plot2Ddata(Nanonisfile, yChns, keepAxes)
     
-#     data_header = Nanonisfile.header
-#     if Nanonisfile.metadata['File extension'] == '.dat'
-#         try :
-#             exp_type = data_header['Experiment']
-#         except :
-#             print('No Experiment type detected. Such information is not in the' 
-#                  'header.')
-#             exp_type = 'generic dat file'
-#     elif Nanonisfile.metadata['File extension'] == '.sxm' :
-#         if Nanonisfile. :
-#             exp_type = data_header
-#         elif
-   
-#     return exp_type
-
-
-
-# def plot_data_from_file(Nanonisfile):
-#     pass
-
-# def alldata(Nanonisfile):
-#     pass
-
-# def channels(Nanonisfile):
-#     pass
-
-
-# Xlabel = "Energy (eV)"
-# Ylabel = "Counter 2 (cps)"
-# Ylabelnorm = "Counts/Current (cps/A)"
-# Xdata = myfile.data[Xlabel]
-# Ydata = myfile.data[Ylabel]
-# Ydatanorm = Ydata/np.abs(myfile.data['Tip Current (A)'])
-
-# fig, ax = plt.subplots()
-# ax.plot(Xdata, Ydata, '.-b', label='row')
-# ax.set_xlabel(Xlabel)
-# ax.set_ylabel(Ylabel)
-
-# ax2 = ax.twinx()
-# ax2.plot(Xdata, Ydatanorm, '.-r', label='norm by current')
-# ax2.set_ylabel(Ylabelnorm)
-
-# plt.title(myfile.metadata["File name"])
-
-# ax.legend(loc=0)
-# ax2.legend(loc=1)
-
-# plt.show()
-
-   
-    
-
