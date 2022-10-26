ab0b7661a39ba006ab946739630dbc6d9a2a7ef4
garghi2000 <garghi2000@hotmail.it>
2022-10-26 11:00:23 +0100

    upgraded inputs handling

--- plot.py
+++ plot.py
@@ -1,25 +1,37 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Sep  9 11:25:02 2022
-
-@author: Gabriele Bertolini
-"""
+from Pynanolib import load
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import numpy as np
 import math
 
-def _plot1Ddata(Nanonisfile, xChn, yChns, keepAxes):
-    data = Nanonisfile.data
-    # some variable for the Width/hight ratio of the plots    
-    unitHeight, goldenRatio = 2, 1.6
-    #extra check for the case xChn is None
-    if '#Index' in data.keys():
+
+def _handle_input_1(Nanonisfile):
+    if not isinstance(Nanonisfile, load.Nanonisfile):
         raise Exception(
-            'The data contains a channel name #Index,'
-            ' this channel might be erased after plot')
-    #check yChns input and convert it in a list of channels name to plot
-    if yChns == 'all' :
+            f"The first input {Nanonisfile} is not a Nanonisfile Object"
+            " of Pynanolib.load module. Please first load your data using"
+            " the load module and pass the Nanonisfile object as first input")
+    return Nanonisfile
+
+
+def _handle_input_2(data, xChn):
+    if xChn is None:
+        # adding a column of indexes to data dict if xChn is not given
+        xName = '#Index'
+        data['#Index'] = np.arange(len(data[list(data.keys())[0]]))
+    elif xChn in data.keys():
+        xName = xChn
+    elif xChn in range(len(data.keys())):
+        xName = list(data.keys())[xChn]
+    else:
+        raise Exception(
+            'The parameter xChn must be either None, a string present in '
+            'the list of the recorded channels names, or an integer')
+    return xName
+
+
+def _handle_input_3(data, yChns):
+    if yChns == 'all':
         yNames = list(data.keys())
     elif yChns in data.keys():
         yNames = [yChns]
@@ -27,46 +39,71 @@ def _plot1Ddata(Nanonisfile, xChn, yChns, keepAxes):
         yNames = yChns
     elif set(yChns).issubset(range(len(data.keys()))):
         yNames = [data.keys()[i] for i in yChns]
-    else :
+    else:
         raise Exception(
             'The parameter yChns must be a list of column names,'
             'a list of idexes, or a string == all')
-        
-    #check on xChn input
-    if xChn == None :
-        # adding a column of indexes to data dict if xChn is not given
-       xName = '#Index'
-       data['#Index'] = np.arange(len(data[list(data.keys())[0]]))
-    elif xChn in data.keys():
-        xName = xChn
-    elif xChn in range(len(data.keys())):
-        xName = list(data.keys())[xChn]
-    else :
+    return yNames
+
+
+def _handle_input_4(keepAxes):
+    if keepAxes not in ['True', 'False', True, False]:
+        raise Exception('The parameter keepAxes must be a boolean')
+    if keepAxes == 'True':
+        keepAxes = True
+    if keepAxes == 'False':
+        keepAxes = False
+    return keepAxes
+
+
+def _handle_input5(figsize):
+    if type(figsize) != tuple or len(figsize) != 2:
         raise Exception(
-            'The parameter xChn must be either None, a string present in '
-            'the list of the recorded channels names, or an integer')
-       
-    #check on keepAxes input
-    if keepAxes == 'True' or keepAxes == True :
+            f"The input figsize = {figsize} is not a tuple of size 2")
+    elif all(type(i) != str for i in figsize):
+        raise Exception(
+            f"The input figsize = {figsize} must be a tuple of 2 numbers"
+            ", not strings")
+    return figsize
+
+
+def _figure_all_in_one():
+    pass
+
+
+def _figures_in_a_grid():
+    pass
+
+
+def _plot1Ddata(nanonisfile, xChn, yChns, keepAxes, figsize):
+    # the order of the _handle_input functions must occur after _handle_input_3
+    nanonisfile = _handle_input_1(nanonisfile)
+    data = nanonisfile.data
+    yNames = _handle_input_3(data, yChns)
+    xName = _handle_input_2(data, xChn)
+    keepAxes = _handle_input_4(keepAxes)
+    unitHeight, goldenRatio = _handle_input5(figsize)
+
+    # check on keepAxes input
+    if keepAxes:
         fig, ax = plt.subplots(figsize=(unitHeight*goldenRatio, unitHeight))
         for yName in yNames:
-            ax.plot(data[xName],data[yName], label = yName)
+            ax.plot(data[xName], data[yName], label=yName)
             plt.legend(loc="upper left")
             plt.xlabel(xName)
         plt.show()
-        
-    elif keepAxes == 'False' or keepAxes == False :
+    elif keepAxes:
         nAxes = len(yNames)
-        #Organizing multiplots such that the figure is as square as possible
-        nRow = math.floor(math.sqrt(nAxes)) 
+        # Organizing multiplots such that the figure is as square as possible
+        nRow = math.floor(math.sqrt(nAxes))
         nCol = math.ceil(nAxes/nRow)
-        #Vector of 2D indexes of the grid in order to use a single loop later
-        gridIdxs = [[i,j] for i in range(nRow) for j in range(nCol)]
+        # Vector of 2D indexes of the grid in order to use a single loop later
+        gridIdxs = [[i, j] for i in range(nRow) for j in range(nCol)]
         wr = [unitHeight*goldenRatio]*nCol
         hr = [unitHeight]*nRow
-        fig = plt.figure(constrained_layout=True, figsize=(sum(wr),sum(hr)))
+        fig = plt.figure(constrained_layout=True, figsize=(sum(wr), sum(hr)))
         grid = gridspec.GridSpec(
-            nRow, nCol, width_ratios= wr, height_ratios= hr, figure = fig
+            nRow, nCol, width_ratios=wr, height_ratios=hr, figure=fig
             )
         for i, yName in enumerate(yNames):
             idx1, idx2 = gridIdxs[i]
@@ -75,21 +112,39 @@ def _plot1Ddata(Nanonisfile, xChn, yChns, keepAxes):
             plt.ylabel(yName)
             plt.xlabel(xName)
         plt.show()
-    else :
-        raise Exception('The parameter keepAxes must be a boolean')
     # removing extra columns of indexes added before if it exists
     if '#Index' in data.keys():
         del data['#Index']
-        
-############ It needed to perform a plot of y vs x chosen channels ######
+
+    return fig
+
+
+# It needed to perform a plot of y vs x chosen channels #
 def _plot2Ddata(Nanonisfile, yChns):
     pass
 
-def plot(Nanonisfile, xChn = None, yChns = 'all', keepAxes = False) :
 
-    if Nanonisfile.metadata['File extension'] == '.dat': 
-        _plot1Ddata(Nanonisfile, xChn, yChns, keepAxes)
-        
-    elif Nanonisfile.metadata['File extension'] == '.sxm':
-        _plot2Ddata(Nanonisfile, yChns, keepAxes)
-    
+def plot(Nanonisfile, xChn=None, yChns='all', keepAxes=False, figsize=(2, 1.6)):
+    """
+    Plot file structured in Nanonisfile class.
+
+    Inputs
+    -------
+        Nanonisfile --> Nanonisfile object (see Pynanonislib.load module).
+        xChn (str or index) --> single channel taken as X-axis.
+        yChns (list of channels, of integers,
+               or single str = 'all', or to a channel name) --> single  or
+                                        multiple channels to plot in Y-axis.
+        keepAxes(boolean or str='False' or 'True') --> option for plotting
+                                                all graphs in a set of axes.
+    Returns
+    -------
+    fig --> figure object (see matlibplot.figure module).
+
+    """
+    plot_case = {".dat": _plot1Ddata, ".sxm": _plot2Ddata}
+    key = Nanonisfile.metadata['File extension']
+    fig = plot_case[key](
+            Nanonisfile, xChn, yChns, keepAxes)
+
+    return fig
